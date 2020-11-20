20bf53c57008b60bc470e158701dfc6be149e224
Anna Filou <38259055+anna-filou@users.noreply.github.com>
2020-11-20 23:51:10 +0200

    re-enabled js cuz faq doesnt work

--- js/webflow.js
+++ js/webflow.js
@@ -15355,11 +15355,11 @@
 			t.evt && t.evt.preventDefault(), t.evt = null
 		}
 		return h.ready = h.design = h.preview = function() {
-			// ! function() {
-			// 	a = t("html").attr("data-wf-site"), o = "https://webflow.com/api/v1/form/" + a, f && o.indexOf("https://webflow.com") >= 0 && (o = o.replace("https://webflow.com", "http://formdata.webflow.com"));
-			// 	if (l = "".concat(o, "/signFile"), !(r = t(p + " form")).length) return;
-			// 	r.each(_)
-            // }
+			! function() {
+				a = t("html").attr("data-wf-site"), o = "https://webflow.com/api/v1/form/" + a, f && o.indexOf("https://webflow.com") >= 0 && (o = o.replace("https://webflow.com", "http://formdata.webflow.com"));
+				if (l = "".concat(o, "/signFile"), !(r = t(p + " form")).length) return;
+				r.each(_)
+            }
             (), g || s || function() {
 				s = !0, c.on("submit", p + " form", function(e) {
 					var r = t.data(this, p);
